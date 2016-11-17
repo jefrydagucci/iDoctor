b9e5dc83e9e2c4abd25db796ce459695f8f4bffc
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-18 04:10:22 +0700

    update profile detail

--- iDoctor/controller/ProfileDetailViewController.m
+++ iDoctor/controller/ProfileDetailViewController.m
@@ -8,8 +8,10 @@
 
 #import "ProfileDetailViewController.h"
 #import "Profile+Request.h"
+#import <MXParallaxHeader/MXParallaxHeader.h>
 
 @interface ProfileDetailViewController ()
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (strong, nonatomic) IBOutlet UIView *headerView;
 @property (strong, nonatomic) IBOutlet UILabel *labelRecommendation;
 @property (strong, nonatomic) IBOutlet UILabel *labelSchedule;
@@ -20,6 +22,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *labelExperience;
 @property (strong, nonatomic) IBOutlet UILabel *labelDescription;
 @property (strong, nonatomic) IBOutlet UILabel *labelLocation;
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
 
 @end
 
@@ -27,7 +30,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    UIScrollView *scrollView = self.scrollView;
+    scrollView.parallaxHeader.view = self.headerView;
+    scrollView.parallaxHeader.height = CGRectGetHeight(self.headerView.bounds);
+    scrollView.parallaxHeader.mode = MXParallaxHeaderModeFill;
+    scrollView.parallaxHeader.minimumHeight = 0;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    NSString *profileId = self.profile.obj_id;
+    [self.view showLoadingWithBlock:^{
+        [Profile getProfileWithPathPattern:[NSString stringWithFormat:@"/test/profile/%@.json",
+                                            profileId] success:^(ASManagedObjectRequestOperation *operation, RKMappingResult *result, NSArray *resultsArray) {
+            Profile *p = resultsArray.firstObject;
+            if([p isKindOfClass:[Profile class]]){
+                [self updateViewWithProfile:p];
+            }
+            
+            [self.view hideActivityView];
+        } failure:^(ASManagedObjectRequestOperation *operation, NSError *error) {
+            ASLog(error.localizedDescription);
+            [self.view hideActivityView];
+        }];
+    }];
 }
 
 - (void)updateViewWithProfile:(Profile *)profile{
@@ -41,6 +69,9 @@
     self.labelSpeciality.text = profile.speciality;
     self.labelDescription.text = profile.obj_description;
     self.labelRecommendation.text = [NSString stringWithFormat:@"%@", profile.recommendation];
+    
+    NSURL *url = [NSURL URLWithString:profile.photo?profile.photo:@""];
+    [self.imgView setImageWithURL:url];
 }
 
 @end
