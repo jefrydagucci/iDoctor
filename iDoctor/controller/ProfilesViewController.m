77e534d5ac993eeb02407cfec56fb116e0bc6ded
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-18 02:41:49 +0700

    add profiles view controller

---
+++ iDoctor/controller/ProfilesViewController.m
@@ -0,0 +1,22 @@
+//
+//  ProfilesViewController.m
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/18/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import "ProfilesViewController.h"
+
+@interface ProfilesViewController ()
+
+@end
+
+@implementation ProfilesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
