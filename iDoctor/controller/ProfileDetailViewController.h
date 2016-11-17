a46f654785b24a7b5bd27852fd71d1b3bbee95a2
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-18 03:31:12 +0700

    add profile detail vc

---
+++ iDoctor/controller/ProfileDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileDetailViewController.h
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/18/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileDetailViewController : UIViewController
+
+@end
---
+++ iDoctor/controller/ProfileDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  ProfileDetailViewController.m
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/18/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import "ProfileDetailViewController.h"
+
+@interface ProfileDetailViewController ()
+
+@end
+
+@implementation ProfileDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
