32b16aa3542cfc173c3251d02155c9ac073da52d
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-17 19:34:51 +0700

    Initial Commit

---
+++ iDoctor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/17/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iDoctor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/17/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
