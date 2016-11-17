054065355c5f7aade0475e3a9069e55b204a871a
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-18 03:07:20 +0700

    add profile cell

---
+++ iDoctor/view/ProfileTableCell.h
@@ -0,0 +1,16 @@
+//
+//  ProfileTableCell.h
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/18/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Profile;
+@interface ProfileTableCell : UITableViewCell
+
+@property (strong, nonatomic) Profile *profile;
+
+@end
---
+++ iDoctor/view/ProfileTableCell.m
@@ -0,0 +1,37 @@
+//
+//  ProfileTableCell.m
+//  iDoctor
+//
+//  Created by Jefry Da Gucci on 11/18/16.
+//  Copyright © 2016 Appslon. All rights reserved.
+//
+
+#import "ProfileTableCell.h"
+#import "Profile.h"
+
+@interface ProfileTableCell ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
+@property (strong, nonatomic) IBOutlet UILabel *labelName;
+@property (strong, nonatomic) IBOutlet UILabel *labelArea;
+@property (strong, nonatomic) IBOutlet UILabel *labelSpeciality;
+@property (strong, nonatomic) IBOutlet UILabel *labelRate;
+
+@end
+
+@implementation ProfileTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setProfile:(Profile *)profile{
+    _profile = profile;
+    self.labelName.text = profile.name;
+    self.labelArea.text = profile.area;
+    self.labelSpeciality.text   = profile.speciality;
+    self.labelRate.text = [NSString stringWithFormat:@"%li", profile.rate.integerValue];
+}
+
+@end
