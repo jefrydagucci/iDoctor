ddf9edcd5254025821fba3520b5667b316402e9d
Jefry Da Gucci <jefrydagucci@gmail.com>
2016-11-18 02:31:29 +0700

    add filtering location

--- iDoctor/model/LocationViewController.m
+++ iDoctor/model/LocationViewController.m
@@ -8,8 +8,13 @@
 
 #import "LocationViewController.h"
 #import "Location+Request.h"
+#import <MXParallaxHeader/MXParallaxHeader.h>
 
 @interface LocationViewController ()
+<UISearchBarDelegate>
+
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *resultsArr;
 
 @end
 
@@ -18,7 +23,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.resultsArr = [NSArray new];
     
+    self.searchBar.delegate = self;
+    
+    UIScrollView *scrollView = self.tableView;
+    scrollView.parallaxHeader.view = self.searchBar;
+    scrollView.parallaxHeader.height = CGRectGetHeight(self.searchBar.bounds);
+    scrollView.parallaxHeader.mode = MXParallaxHeaderModeFill;
+    scrollView.parallaxHeader.minimumHeight = scrollView.parallaxHeader.height;
 }
 
 - (void)getContentWithRefreshContentType:(RefreshContentType)refreshContentType{
@@ -28,8 +41,9 @@
         
         [Location getLocationWithPathPattern:@"/test/location.json" success:^(ASManagedObjectRequestOperation *operation, RKMappingResult *result, NSArray *resultsArray) {
             
-            [self processingSuccessWithOperation:operation result:result arrData:resultsArray refreshContentType:refreshContentType tableView:self.tableView completion:^{
-                
+            self.resultsArr = resultsArray;
+            
+            [self processingResultArray:resultsArray successWithOperation:operation result:result refreshContentType:refreshContentType completion:^{
                 dispatch_async(dispatch_get_main_queue(), ^{
                     [self.view hideActivityView];
                     [self.tableView reloadData];
@@ -45,6 +59,17 @@
     }];
 }
 
+- (void)processingResultArray:(NSArray *)array
+         successWithOperation:(ASManagedObjectRequestOperation *)operation
+                       result:(RKMappingResult *)result
+           refreshContentType:(RefreshContentType)refreshContentType
+                   completion:(void(^)(void))completion{
+    [self processingSuccessWithOperation:operation result:result arrData:array refreshContentType:refreshContentType tableView:self.tableView completion:^{
+        
+        !completion?:completion();
+    }];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *cellIdentifier = @"cellIdentifier";
     UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
@@ -56,8 +81,35 @@
     cell.textLabel.text = [NSString stringWithFormat:@"%@, %@",
                            l.area?l.area:@"",
                            l.city?l.city:@""];
-    
     return cell;
+}
+
+#pragma mark - filter
+- (void)filterByText:(NSString *)text{
+    NSPredicate *pred       = [NSPredicate predicateWithFormat:@"(area CONTAINS[cd] %@ or area like %@) or (city like %@ or city CONTAINS[cd] %@)", text, text, text, text];
+    
+    NSArray *filtered = text.length>1?[self.resultsArr filteredArrayUsingPredicate:pred]:self.resultsArr;
+    
+    
+    [self processingResultArray:filtered successWithOperation:nil result:nil refreshContentType:RefreshContentTypeReplaceAll completion:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.view hideActivityView];
+            [self.tableView reloadData];
+        });
+    }];
+}
+
+#pragma mark - UISearchBarDelegate;
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    NSString *filterText    = searchText;
+    [self filterByText:filterText];
+}
+
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    [self.view endEditing:YES];
+    NSString *filterText    = self.searchBar.text;
+    [self filterByText:filterText];
 }
 
 @end
